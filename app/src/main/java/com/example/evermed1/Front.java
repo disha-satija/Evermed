a04db811220c527b0b9a02c2dcef2d39dd883d4c
Disha <disha@Dishas-MacBook-Air.local>
2025-04-19 22:32:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/evermed1/Front.java
@@ -0,0 +1,37 @@
+package com.example.evermed1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Front extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_front);
+
+        View mainLayout = findViewById(R.id.main);
+
+        // Insets setup (optional, for full-screen)
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mainLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(Front.this, LoginActivity.class);
+
+            startActivity(intent);
+            // Optional: closes the splash screen
+        });
+    }
+}
