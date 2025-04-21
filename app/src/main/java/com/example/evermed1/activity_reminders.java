ae226602cd55fe86b267a7595792bd010e3c6f67
Disha Satija <dishasatija20@gmail.com>
2025-04-21 14:11:57 +0530

    Signup Page

--- app/src/main/java/com/example/evermed1/activity_reminders.java
+++ app/src/main/java/com/example/evermed1/activity_reminders.java
@@ -1,9 +1,6 @@
 package com.example.evermed1;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class activity_reminders extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
-        // Apply insets
+        setContentView(R.layout.activity_reminders);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        // Handle signup link click
-        TextView signupLink = findViewById(R.id.signup_link);
-        signupLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start the Signup Activity
-                Intent intent = new Intent(LoginActivity.this, activity_signup.class);
-                startActivity(intent);
-            }
         });
     }
 }
